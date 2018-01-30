72ee8e3d7a35831f9ec96a5bdf172df95de9aa47
Reflejo <tocinonaro_juan@yahoo.com.ar>
2018-01-30 17:49:52 -0300

    Webmethod Set_Usuario y Set_Grupo
    Webmethod Set_Usuario y Set_Grupo

--- Sisa/Services/Service.asmx.cs
+++ Sisa/Services/Service.asmx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using Business;
 
 namespace Sisa.Services
 {
@@ -16,10 +17,23 @@ namespace Sisa.Services
      [System.Web.Script.Services.ScriptService]
     public class Service : System.Web.Services.WebService
     {
+        public O_Business O_Business = null; // Creo un objeto global (en común a todos los webmethod).
         [WebMethod(CacheDuration = 1, BufferResponse = false)]
         public string Login(string user, string pass)
         {
             
+        }
+        public string Set_Usuario(string usuario, string password, string email)
+        {
+            O_Business = new O_Business(); // Inicializo el objeto global.
+            return O_Business.Set_Usuario(usuario, password, email); // Devuelvo el string del estado de la operación.
+
+        }
+        [WebMethod]
+        public string Set_Grupo(string nombre, string descripcion, int administrador_id)
+        {
+            O_Business = new O_Business(); // Inicializo el objeto global.
+            return O_Business.Set_Grupo(nombre, descripcion, administrador_id); // Devuelvo el OBJETO persona a la vista HTML.
         }
     }
 }
