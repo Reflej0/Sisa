1ded57806f491f619e8f67c0cc6052ecaa3ebf7b
Reflejo <tocinonaro_juan@yahoo.com.ar>
2018-02-09 16:41:17 -0300

    Actualizados todos los SP de Insert
    SELECT SCOPE_IDENTITY() para retorar el ID del último elemento ingresado.

--- BD/Sisa_Procedimientos_Almacenados.sql
+++ BD/Sisa_Procedimientos_Almacenados.sql
@@ -136,18 +136,21 @@ CREATE PROCEDURE Set_Usuario @v_Usuario varchar(255), @v_Password varchar(255), 
 AS
 BEGIN
     INSERT INTO Usuarios(usuario, password, email) VALUES (@v_Usuario, @v_Password, @v_Email)
+    SELECT SCOPE_IDENTITY()
 END
 
 CREATE PROCEDURE Set_Usuario_Grupo @v_Grupo_id int, @v_Usuario_id int
 AS
 BEGIN
     INSERT INTO Grupos_Usuarios(grupo_id, usuario_id) VALUES (@v_Grupo_id, @v_Usuario_id)
+    SELECT SCOPE_IDENTITY()
 END
 
 CREATE PROCEDURE Set_Sancion @v_Grupo_id int, @v_Usuario_creador_id int, @v_Usuario_sancionado_id int, @v_Motivo varchar(255), @v_Estado int, @v_Fecha_creacion DATETIME
 AS
 BEGIN
     INSERT INTO Sanciones(grupo_id, usuario_creador_id, usuario_sancionado_id, motivo, estado, fecha_creacion) VALUES (@v_Grupo_id, @v_Usuario_creador_id, @v_Usuario_sancionado_id, @v_Motivo, @v_Estado, @v_Fecha_creacion)
+    SELECT SCOPE_IDENTITY()
 END
 
 CREATE PROCEDURE Update_Sancion @v_Sancion_id int
